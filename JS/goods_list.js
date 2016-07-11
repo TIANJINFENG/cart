7eb1af9de52b432da47c8fa0759a444ba09fe613
TIANJINFENG <727798013@qq.com>
2016-07-11 17:08:57 +0800

    then

--- JS/goods_list.js
+++ JS/goods_list.js
@@ -1,8 +1,26 @@
 /**
  * Created by tianjinfeng on 16-7-1.
  */
-var count=0;
-goodlist()
+$(document).ready(function(){
+//window.onload = function(){
+    a();
+    goodlist();
+});
+function a() {
+        if (localStorage.getItem("clicks") == null) {
+            localStorage.setItem("clicks", 0);
+        }
+        var num = localStorage.getItem("clicks");
+        $("#plus").text(num);
+    }
+$(document).ready(function() {
+    $(".add_cart_button").click(function () {
+        var count = $("#plus").text();
+        count = parseInt(count) + 1;
+        localStorage.setItem("clicks", count);
+        $("#plus").text(count);
+    });
+})
 function goodlist() {
     var a=[{"分类":"饮料","名称":"可口可乐","单价":3,"单位":"瓶"},
         {"分类":"饮料","名称":"雪碧","单价":3,"单位":"瓶"},
@@ -10,28 +28,9 @@ function goodlist() {
         {"分类":"水果","名称":"荔枝","单价":15,"单位":"斤"},
         {"分类":"生活用品","名称":"电池","单价":2,"单位":"个"},
         {"分类":"食品","名称":"方便面","单价":4.5,"单位":"袋"}];
-    count = parseInt(count)+1;
-    if(count==1){
-        for(var i=0;i<a.length;i++) {
-            //获取行下标
-            var rowIndex = document.getElementById("order").rows.length;
-//插入新行
-            var newRow = document.getElementById("order").lastChild.insertRow(rowIndex);
-//插入4个列
-            var cell1 = newRow.insertCell(0);
-            var cell2 = newRow.insertCell(1);
-            var cell3 = newRow.insertCell(2);
-            var cell4 = newRow.insertCell(3);
-            var cell5 = newRow.insertCell(4);
-
-//设置行id
-            //newRow.id = "row" + rowIndex;
-//设置列属性
-            cell1.innerHTML = a[i].分类;
-            cell2.innerHTML = a[i].名称;
-            cell3.innerHTML = a[i].单价;
-            cell4.innerHTML = a[i].单位;
-            cell5.innerHTML = "<button class='addcartbutton' onclick='listcount()'>加入购物车</button>";
-        }
+    for(var i=0;i<a.length;i++) {
+       var tr="<tr class='list'><td>"+a[i].分类+"</td><td>"+a[i].名称+"</td><td>"+a[i].单价+"</td><td>"+a[i].单位+"</td>" +
+           "<td><button class='add_cart_button'>加入购物车</button></td></tr>"
+        $("table").append(tr);
     }
 }
--- JS/posHomepage.js
+++ JS/posHomepage.js
@@ -1,3 +1,18 @@
 /**
  * Created by tianjinfeng on 16-7-11.
  */
+window.onload = function () {
+    a();
+}
+function a(){
+    if (localStorage.getItem("clicks") == null) {
+        loscalStorage.setItem("clicks", 0);
+    }
+    document.getElementById("plus").innerHTML = localStorage.getItem("clicks");
+}
+function listcount() {
+    var count = document.getElementById("plus").innerHTML;
+    count = parseInt(count) + 1;
+    localStorage.setItem("clicks", count);
+    document.getElementById("plus").innerHTML = localStorage.getItem("clicks");
+}
