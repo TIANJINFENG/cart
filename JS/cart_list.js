3c4487b7c71b5bb3d26ec373299458900260ca15
TIANJINFENG <727798013@qq.com>
2016-07-14 11:34:41 +0800

    4

--- JS/cart_list.js
+++ JS/cart_list.js
@@ -3,24 +3,60 @@
  */
 $(document).ready(function(){
     add_count();
+    add_count_cart();
+    shopping();
+    add_cart_goods_amount();
+    add_quntitey();
+    reduce_quntitey();
 });
 function add_count() {
     if (localStorage.getItem("clicks") == null) {
         localStorage.setItem("clicks", 0);
     }
     var num = localStorage.getItem("clicks");
-    $("#plus").text(num);
+    $("#number").text(num);
+    $("#amount").text(num);
 }
-$(document).ready(function() {
-    $(".add_cart_button").click(function () {
-        var count = $("#plus").text();
+function add_cart_goods_amount() {
+    if (localStorage.getItem("clicks") == null) {
+        localStorage.setItem("clicks", 0);
+    }
+    var num = localStorage.getItem("cart_goods_amount");
+    $("#amount").text(num);
+}
+function add_count_cart() {
+    $(".add_order_button").click(function () {
+        var count = $("#number").text();
         count = parseInt(count) + 1;
         localStorage.setItem("clicks", count);
-        $("#plus").text(count);
+        $("#number").text(count);
     });
-});
-function shopping(){
-    $(".add_cart_button").click(function () {
-
-    })
+};
+function add_quntitey() {
+    $(".add_count").click(function () {
+        var count = $("#amount").html();
+        count = parseInt(count) + 1;
+        localStorage.setItem("cart_goods_amount", count);
+        var add_num= localStorage.getItem("cart_goods_amount")
+        $("#amount").html(add_num);
+    });
+}
+function reduce_quntitey() {
+    $(".reduce_count").click(function () {
+        var count = $("#amount").html();
+        if(count>=1){
+        count = parseInt(count) - 1;
+        localStorage.setItem("cart_goods_amount", count);
+        $("#amount").val(count);
+        }
+    });
+}
+function shopping() {
+    for(var i=1;i<6;i++){
+    var shopping_string = $(".cart_item_template").html();
+    var l_string = localStorage.getItem("ID"+i);
+    var l=JSON.parse(l_string);
+    var shopping_item = shopping_string.replace(/kind/,l.分类).replace(/name/,l.名称).replace(/price/,l.单价).replace(/print/, l.单位).replace(/"0"/, l.count);
+    $(".orders_table").append(shopping_item);
+}
 }
--- JS/goods_list.js
+++ JS/goods_list.js
@@ -2,21 +2,36 @@
  * Created by tianjinfeng on 16-7-1.
  */
 $(document).ready(function(){
-    var item =[{"分类":"饮料","名称":"可口可乐","单价":3,"单位":"瓶"},
-        {"分类":"饮料","名称":"雪碧","单价":3,"单位":"瓶"},
-        {"分类":"水果","名称":"苹果","单价":5.5,"单位":"斤"},
-        {"分类":"水果","名称":"荔枝","单价":15,"单位":"斤"},
-        {"分类":"生活用品","名称":"电池","单价":2,"单位":"个"},
-        {"分类":"食品","名称":"方便面","单价":4.5,"单位":"袋"}];
+    var item =[{"分类":"饮料","名称":"可口可乐","单价":3,"单位":"瓶",ID:1},
+        {"分类":"饮料","名称":"雪碧","单价":3,"单位":"瓶",ID:2},
+        {"分类":"水果","名称":"苹果","单价":5.5,"单位":"斤",ID:3},
+        {"分类":"水果","名称":"荔枝","单价":15,"单位":"斤",ID:4},
+        {"分类":"生活用品","名称":"电池","单价":2,"单位":"个",ID:5},
+        {"分类":"食品","名称":"方便面","单价":4.5,"单位":"袋",ID:6}];
     show_count();
     show_good_list(item);
     add_count_cart();
+    list(item);
 });
-/*function list(item){
-        item[1] = JSON.stringify(item[1])
-        localStorage.setItem("click", item[1]);
-        $("#orders").html(item[1]);
-}*/
+function list(item){
+    for(var i=0;i<item.length;i++){
+    listtt(i+1,item[i]);
+    }
+}
+function listtt(button, item){
+    $("#"+button+"").click(function (){
+        if(localStorage.getItem("ID"+button)==null){
+            item.count=1;
+            item_string = JSON.stringify(item);
+            localStorage.setItem("ID"+button,item_string);
+        }
+        else{
+            item.count++;
+            item_string = JSON.stringify(item);
+            localStorage.setItem("ID"+button,item_string);
+        }
+    });
+}
 function show_count() {
         if (localStorage.getItem("clicks") == null) {
             localStorage.setItem("clicks", 0);
@@ -35,7 +50,7 @@ function add_count_cart() {
 function show_good_list(item) {
     for(var info=0;info<item.length;info++) {
         var shopping_string = $(".item_template").html();
-        var shopping_item=shopping_string.replace(/kind/,item[info].分类).replace(/name/,item[info].名称).replace(/price/,item[info].单价).replace(/print/,item[info].单位)+"\n";
+        var shopping_item=shopping_string.replace(/kind/,item[info].分类).replace(/name/,item[info].名称).replace(/price/,item[info].单价).replace(/print/,item[info].单位).replace(/"b"/,item[info].ID);
         $(".inventory_table").append(shopping_item);
     }
 }
