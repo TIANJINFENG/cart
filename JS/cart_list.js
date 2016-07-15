ce22ff14b322ae6a4f6a686e4d812e1b693dd9b0
TIANJINFENG <727798013@qq.com>
2016-07-15 18:08:38 +0800

    算小计

--- JS/cart_list.js
+++ JS/cart_list.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     add_count_cart();
     shopping();
     chang_shopping_count();
-
 });
 function Items_barcode(){
     var cart_items_barcade_string=localStorage.getItem("items_barcade");
@@ -37,6 +36,7 @@ function chang_shopping_count(){
 function add_quntitey(cart_items_barcade) {
     $(".add_count#"+cart_items_barcade+"").click(function () {
         var plus_count = $("input#"+cart_items_barcade+"").val();
+        var little_price = $(".goods_little_price#"+cart_items_barcade+"").html();
           plus_count = parseInt(plus_count) + 1;
         var num = $("#number").text();
         num = parseInt(num) + 1;
@@ -46,21 +46,38 @@ function add_quntitey(cart_items_barcade) {
         $("input#"+ cart_items_barcade+"").val(plus_count);//在文本显示数量
         var add_string = localStorage.getItem("cart"+cart_items_barcade);
         var cart_items=JSON.parse(add_string);
+        var total_shopping_money=$("#sum_goods_money").html();
+        var money=parseInt(total_shopping_money)+cart_items.单价;
+            localStorage.setItem("total",money);
+        var total_money = localStorage.getItem("total");
+        $("#sum_goods_money").html(parseInt(total_money).toFixed(2));
         cart_items.count=plus_count;//改变本地商品数量
-        cart_items_string=JSON.stringify(cart_items);
+        little_price = plus_count*cart_items.单价;
+        cart_items.original_price=little_price;//改变本地小计价格
+        $(".goods_little_price#"+cart_items_barcade+"").html(cart_items.original_price);
+
+            cart_items_string=JSON.stringify(cart_items);
         localStorage.setItem("cart"+cart_items_barcade, cart_items_string);
     });
 }
 function reduce_quntitey(cart_items_barcade) {
     $(".reduce_count#"+cart_items_barcade+"").click(function () {
         var cut_count = $("input#"+cart_items_barcade+"").val();
+        var little_price = $(".goods_little_price#"+cart_items_barcade+"").html();
         if(cut_count>1){
         cut_count = parseInt(cut_count) - 1;
         $("input#"+ cart_items_barcade+"").val(cut_count);//在文本显示数量
-            //goods_sum_price=
         var add_string = localStorage.getItem("cart"+cart_items_barcade);
         var cart_items=JSON.parse(add_string);
-        cart_items.count=cut_count;//改变本地商品数量
+            var total_shopping_money=$("#sum_goods_money").html();
+            var money=parseInt(total_shopping_money)-cart_items.单价;
+            localStorage.setItem("total",money);
+            var total_money = localStorage.getItem("total");
+            $("#sum_goods_money").html(parseInt(total_money).toFixed(2));
+            cart_items.count=cut_count;//改变本地商品数量
+            little_price = cut_count*cart_items.单价;
+            cart_items.original_price=little_price;//改变本地小计价格
+            $(".goods_little_price#"+cart_items_barcade+"").html(cart_items.original_price);
         cart_items_string=JSON.stringify(cart_items);
         localStorage.setItem("cart"+cart_items_barcade, cart_items_string);
         }
@@ -78,6 +95,7 @@ function reduce_quntitey(cart_items_barcade) {
 }
 function shopping() {
     var cart_items_barcade=Items_barcode();
+    var goods_sum_price=0;
     for(var i=0;i<cart_items_barcade.length;i++){
         if(localStorage.getItem("cart"+cart_items_barcade[i])==null){continue ;}
     var shopping_string = $(".cart_item_template").html();
@@ -87,11 +105,14 @@ function shopping() {
         .replace(/name/,l.名称)
         .replace(/price/,l.单价)
         .replace(/print/, l.单位)
-        .replace(/q/, l.ID).replace(/increat/, l.ID).replace(/reduces/, l.ID).replace(/delate/, l.ID);
+        .replace(/q/, l.ID).replace(/increat/, l.ID).replace(/reduces/, l.ID)
+        .replace(/delate/, l.ID).replace(/goods_original_price/,l.ID);
         $(".orders_table").append(shopping_item);
         $("input#"+ cart_items_barcade[i]+"").val(l.count);
+        $(".goods_little_price#"+cart_items_barcade[i]+"").html(l.original_price);
+        var tian=l.original_price;
+        goods_sum_price+=tian;
     }
+    $("#sum_goods_money").html((goods_sum_price).toFixed(2));
+    localStorage.setItem("sum",goods_sum_price.toFixed(2));
 }
-/*function sum_price(){
-    var goods_sum_price=
-}*/
