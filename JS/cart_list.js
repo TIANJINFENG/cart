ca99e278a959b7e30eb8e6e335effc2ae4df0894
TIANJINFENG <727798013@qq.com>
2016-07-26 17:40:56 +0800

    ming zi

--- JS/cart_list.js
+++ JS/cart_list.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
     bind_all_add_and_dec_button();
 });
 function load_goods_initial_items(){
-    var cart_items_barcade=get_local_storage("items_barcade");
+    var cart_items_barcade=get_local_string("items_barcade");
     var goods_total = 0;
     $.each(cart_items_barcade,function(index,id){
-        var local_items = get_local_storage("cart"+id);
+        var local_items = get_local_string("cart"+id);
         if(local_items!=null){
             goods_total += local_items.original_price;
             replace_goods_items(local_items,id);
@@ -30,7 +30,7 @@ function replace_goods_items(local_items,id) {
     update(id);
 }
 function bind_all_add_and_dec_button(){
-    var cart_items_barcade=get_local_storage("items_barcade")
+    var cart_items_barcade=get_local_string("items_barcade")
     $.each(cart_items_barcade,function(i){
         bind_add_and_dec_button_function(this);
     });
@@ -38,7 +38,7 @@ function bind_all_add_and_dec_button(){
 function bind_add_and_dec_button_function(cart_items_barcade){
     $("#"+cart_items_barcade+" .reduce_count , #"+cart_items_barcade+" .add_count").click(function (event){
         var num=(event.target.className== "add_count")? 1:-1;
-        var cart_item=get_local_storage("cart"+cart_items_barcade);
+        var cart_item=get_local_string("cart"+cart_items_barcade);
         var cart_items=compute_goods_items(cart_item,num);
         compute_total_and_sum(num,cart_items,cart_items_barcade);
         update(cart_items_barcade);
@@ -46,8 +46,8 @@ function bind_add_and_dec_button_function(cart_items_barcade){
     });
 }
 function update(cart_items_barcade){
-    var cart_items=get_local_storage("cart"+cart_items_barcade)
-    show_goods_items(cart_items_barcade,cart_items);
+    var cart_items=get_local_string("cart"+cart_items_barcade)
+    show_goods_count_and_sub_total(cart_items_barcade,cart_items);
     show_goods_total();
     show_cart_goods_count();
 }
@@ -71,24 +71,24 @@ function compute_goods_items(cart_items,num){
     return cart_items;
 }
 function compute_shopping_total(cart_items,num){
-    var  money=Number(get_local("total"));
+    var  money=Number(get_local_value("total"));
     var total_money=money+(num*cart_items.price);
     set_local("total",total_money.toFixed(2));
 }
 function compute_cart_count(num){
-    var cart_number = get_local_storage("count");
+    var cart_number = get_local_string("count");
     if(cart_number>0){
         cart_number = parseInt(cart_number) + num;
     }
     set_local("count", cart_number);
     if(cart_number==0){window.location.href="goods_list.html"}
 }
-function show_goods_items(cart_items_barcade,local_items){
+function show_goods_count_and_sub_total(cart_items_barcade,local_items){
     show_every_quntity(cart_items_barcade,local_items.count);
-    show_shopping_little_price(local_items,cart_items_barcade);
+    show_shopping_sub_total(local_items,cart_items_barcade);
 }
-function show_shopping_little_price(local_items,cart_items_barcade){
-    var preferential_items=get_local_storage("preferential_items_id")
+function show_shopping_sub_total(local_items,cart_items_barcade){
+    var preferential_items=get_local_string("preferential_items_id")
     if(preferential_items.indexOf(local_items.ID)>-1&&local_items.count>=3){
         show("#"+ cart_items_barcade+" .goods_little_price",local_items.save+"元"+"(原价:"+local_items.original_price+"元)");
     }else {
@@ -96,21 +96,21 @@ function show_shopping_little_price(local_items,cart_items_barcade){
     }
 }
 function show_goods_total(){
-    var money=Number(get_local("total"));
+    var money=Number(get_local_value("total"));
     show("#sum_goods_money",Number(money).toFixed(2));
 }
 function show_cart_goods_count(){
-    var cart_number = get_local_storage("count")||0;
+    var cart_number = get_local_string("count")||0;
     show("#number",cart_number);
 }
 function set_local(key,item){
     localStorage.setItem(key,item);
 }
-function get_local(key){
+function get_local_value(key){
     var value = localStorage.getItem(key);
     return value;
 }
-function get_local_storage(key){
+function get_local_string(key){
     var value_string = localStorage.getItem(key);
     var cart_items=JSON.parse(value_string);
     return cart_items;
