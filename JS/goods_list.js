5c9f3ca245bda9e8cffcb3f67649f6ce003215d2
TIANJINFENG <727798013@qq.com>
2016-07-26 17:51:46 +0800

    all js

--- JS/goods_list.js
+++ JS/goods_list.js
@@ -2,35 +2,50 @@
  * Created by tianjinfeng on 16-7-1.
  */
 $(document).ready(function(){
-    var item =[{"分类":"饮料","名称":"可口可乐","单价":3,"单位":"瓶",ID:1},
-        {"分类":"饮料","名称":"雪碧","单价":3,"单位":"瓶",ID:2},
-        {"分类":"水果","名称":"苹果","单价":5.5,"单位":"斤",ID:3},
-        {"分类":"水果","名称":"荔枝","单价":15,"单位":"斤",ID:4},
-        {"分类":"生活用品","名称":"电池","单价":2,"单位":"个",ID:5},
-        {"分类":"食品","名称":"方便面","单价":4.5,"单位":"袋",ID:6}];
+    var item =[{"kinds":"饮料","name":"可口可乐","price":3,"unit":"瓶",ID:"barcade1"},
+        {"kinds":"饮料","name":"雪碧","price":3,"unit":"瓶",ID:"barcade2"},
+        {"kinds":"水果","name":"苹果","price":5.5,"unit":"斤",ID:"barcade3"},
+        {"kinds":"水果","name":"荔枝","price":15,"unit":"斤",ID:"barcade4"},
+        {"kinds":"生活用品","name":"电池","price":2,"unit":"个",ID:"barcade5"},
+        {"kinds":"食品","name":"方便面","price":4.5,"unit":"袋",ID:"barcade6"}];
+    var preferential_items=["barcade1","barcade2","barcade6"];
     show_count();
     show_good_list(item);
     add_count_cart();
-    list(item);
+    list(item,preferential_items);
 });
-function list(item){
+function list(item,preferential_items){
+    var items_ID=[];
     for(var i=0;i<item.length;i++){
-    listtt(i+1,item[i]);
+        add_goods_cart(item[i].ID,item[i]);
+        items_ID.push(item[i].ID);
     }
+    var items_ID_string=JSON.stringify(items_ID)
+    var preferential_items_ID=JSON.stringify(preferential_items)
+    localStorage.setItem("items_barcade",items_ID_string);
+    localStorage.setItem("preferential_items_id",preferential_items_ID)
 }
-function listtt(button, item){
+function add_goods_cart(button, item){
     $("#"+button+"").click(function (){
-        if(localStorage.getItem("ID"+button)==null){
-            item.count=1;
+        if(localStorage.getItem("cart"+button)==null){
+            item.count = 1;
+            item.original_price = (item.count*item.price);
+            if(item.unit!="斤"){
+                item.save = (item.count-parseInt(item.count/3))*item.price;
+            }
             item_string = JSON.stringify(item);
-            localStorage.setItem("ID"+button,item_string);
+            localStorage.setItem("cart"+button,item_string);
         }
           else{
-            var counts= localStorage.getItem("ID"+button);
+            var counts= localStorage.getItem("cart"+button);
             counts=JSON.parse( counts);
             counts.count++;
+            counts.original_price=(counts.count*counts.price);
+            if(counts.unit!="斤"){
+                counts.save = (counts.count-parseInt(counts.count/3))*counts.price;
+            }
             item_string = JSON.stringify(counts);
-            localStorage.setItem("ID"+button,item_string);
+            localStorage.setItem("cart"+button,item_string);
         }
     });
 }
@@ -52,7 +67,11 @@ function add_count_cart() {
 function show_good_list(item) {
     for(var info=0;info<item.length;info++) {
         var shopping_string = $(".item_template").html();
-        var shopping_item=shopping_string.replace(/kind/,item[info].分类).replace(/name/,item[info].名称).replace(/price/,item[info].单价).replace(/print/,item[info].单位).replace(/"b"/,item[info].ID);
+        var shopping_item=shopping_string.replace(/kind/,item[info].kinds)
+            .replace(/name/,item[info].name)
+            .replace(/price/,item[info].price)
+            .replace(/unit/,item[info].unit)
+            .replace(/"b"/,item[info].ID);
         $(".inventory_table").append(shopping_item);
     }
 }
--- JS/posHomepage.js
+++ JS/posHomepage.js
@@ -2,21 +2,11 @@
  * Created by tianjinfeng on 16-7-11.
  */
 $(document).ready(function(){
-
     a();
 });
 function a() {
-    if (localStorage.getItem("clicks") == null) {
-        localStorage.setItem("clicks", 0);
-    }
-    var num = localStorage.getItem("clicks");
+
+    var num = localStorage.getItem("clicks")||0;
     $("#plus").text(num);
 }
-$(document).ready(function() {
-    $(".add_cart_button").click(function () {
-        var count = $("#plus").text();
-        count = parseInt(count) + 1;
-        localStorage.setItem("clicks", count);
-        $("#plus").text(count);
-    });
-});
+
